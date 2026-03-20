96297fa93b1a5177776466fed326e760cc508253
[kleyacabatu] <[kleya.cabatu118@gmail.com]>
2026-03-20 15:43:47 +0800

    Delete Organization feature is working

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy 
@@ -29,3 +29,8 @@ class OrganizationUpdateView(UpdateView):
      form_class = OrganizationForm  
      template_name = 'org_form.html'  
      success_url = reverse_lazy('organization-list')
+
+class OrganizationDeleteView(DeleteView):  
+     model = Organization  
+     template_name = 'org_del.html'  
+     success_url = reverse_lazy('organization-list') 
